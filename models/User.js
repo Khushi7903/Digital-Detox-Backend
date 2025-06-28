d6562ea9aa141a4d230a8b53ed5ff12bb3896cf4
Khushi Babbar <khushibabbar2020@gmail.com>
2025-06-28 09:48:54 +0530

    phone no added in user model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  role: { type: String, enum: ["student", "teacher","mentor", "buddy"], required: true },
+  role: {
+    type: String,
+    enum: ["student", "teacher", "mentor", "buddy", "parent"],
+    required: true,
+  },
   name: { type: String, required: true },
   id: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
-
 
 module.exports = mongoose.model("User", userSchema);
