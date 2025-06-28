b315d8c31a271c590be01027c92aaf277931a746
Khushi Babbar <khushibabbar2020@gmail.com>
2025-06-28 11:24:58 +0530

    commit

--- models/User.js
+++ models/User.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  role: {
-    type: String,
-    enum: ["student", "teacher", "mentor", "buddy", "parent"],
-    required: true,
-  },
+  role: { type: String, enum: ["student", "teacher","mentor", "buddy"], required: true },
   name: { type: String, required: true },
   id: { type: String, required: true, unique: true },
-  phone: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
 
 module.exports = mongoose.model("User", userSchema);
